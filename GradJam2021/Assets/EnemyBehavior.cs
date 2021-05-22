acaddd2a2c45cf6ebd798e73b53795220f1dbdb8
JamesCNunes <55606935+JamesCNunes@users.noreply.github.com>
2021-05-22 12:23:11 -0400

    Enemy moving

---
+++ GradJam2021/Assets/EnemyBehavior.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class EnemyBehavior : MonoBehaviour
+{
+
+    [SerializeField]private float speed;
+    [SerializeField]private float groundCheckLength;
+    [SerializeField]private bool moveLeft;
+    [SerializeField]private BoxCollider2D boxCol;
+    [SerializeField]private LayerMask mask;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (moveLeft)
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(new Vector2(boxCol.bounds.min.x, boxCol.bounds.min.y), Vector2.down, groundCheckLength, mask);
+            Debug.DrawRay(new Vector2(boxCol.bounds.min.x, boxCol.bounds.min.y), Vector2.down * groundCheckLength, Color.red);
+            
+            if (hit.collider == null)
+            {
+                moveLeft = false;
+            }
+        }
+        else
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(new Vector2(boxCol.bounds.max.x, boxCol.bounds.min.y), Vector2.down, groundCheckLength, mask);
+            Debug.DrawRay(new Vector2(boxCol.bounds.max.x, boxCol.bounds.min.y), Vector2.down * groundCheckLength, Color.red);
+
+            if (hit.collider == null)
+            {
+                moveLeft = true;
+            }
+        }
+    }
+}
