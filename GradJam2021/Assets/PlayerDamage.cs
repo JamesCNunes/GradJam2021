de8c747e37fee25e391fe02c23e6a1dc2904365b
JamesCNunes <55606935+JamesCNunes@users.noreply.github.com>
2021-05-22 11:33:53 -0400

    Player Damaged by enemy

---
+++ GradJam2021/Assets/PlayerDamage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDamage : MonoBehaviour
+{
+    [SerializeField] private Health playerHealth;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            playerHealth.TakeDamage();
+            Debug.Log("Player Damaged");
+        }
+    }
+}
