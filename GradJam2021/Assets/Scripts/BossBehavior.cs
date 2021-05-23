2cdaabd89cbe2a7daae9a2ae91b44705eae0f3a4
JamesCNunes <55606935+JamesCNunes@users.noreply.github.com>
2021-05-23 14:31:12 -0400

    Working on Mammoth

--- GradJam2021/Assets/Scripts/BossBehavior.cs
+++ GradJam2021/Assets/Scripts/BossBehavior.cs
@@ -13,8 +13,14 @@ public class BossBehavior : MonoBehaviour
         Dead
     }
 
-    private Mammoth state = Mammoth.WaitToStart;
+    private Mammoth state = Mammoth.Charging;
+    bool facingLeft;
 
+    [SerializeField] private BoxCollider2D box;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float chargeSpeed;
+    [SerializeField] private float checkDistance;
+    [SerializeField] private LayerMask mask;
 
     private void Update()
     {
@@ -25,6 +31,7 @@ public class BossBehavior : MonoBehaviour
             case Mammoth.Idle:
                 break;
             case Mammoth.Charging:
+                Charging();
                 break;
             case Mammoth.Stunned:
                 break;
@@ -36,8 +43,50 @@ public class BossBehavior : MonoBehaviour
         }
     }
 
+    public void StartBattle()
+    {
+        state = Mammoth.Charging;
+    }
+
     public Mammoth GetState()
     {
         return state;
+    }
+
+    private void Charging()
+    {
+
+        RaycastHit2D hit;
+
+        if (facingLeft)
+        {
+            hit = Physics2D.Raycast(new Vector2(box.bounds.max.x, box.bounds.center.y), Vector2.left, checkDistance, mask);
+            Debug.DrawRay(new Vector2(box.bounds.max.x, box.bounds.center.y), Vector2.left * checkDistance, Color.red);
+        }
+        else
+        {
+            hit = Physics2D.Raycast(new Vector2(box.bounds.min.x, box.bounds.center.y), Vector2.right, checkDistance, mask);
+            Debug.DrawRay(new Vector2(box.bounds.min.x, box.bounds.center.y), Vector2.right * checkDistance, Color.red);
+        }
+
+        if(hit.collider != null)
+        {
+            //Hit the wall
+            
+
+            facingLeft = !facingLeft;
+            this.gameObject.transform.localScale = new Vector3(this.gameObject.transform.localScale.x * -1, this.gameObject.transform.localScale.y, this.gameObject.transform.localScale.z);
+        }
+        
+
+        if (facingLeft)
+        {
+            rb.velocity = new Vector2(chargeSpeed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-chargeSpeed, rb.velocity.y);
+        }
+
     }
 }
--- GradJam2021/Assets/Scripts/JumpPad.cs
+++ GradJam2021/Assets/Scripts/JumpPad.cs
@@ -7,10 +7,13 @@ public class JumpPad : MonoBehaviour
     [SerializeField] private float force;
     Rigidbody2D rb;
 
+    [SerializeField] private Animator anim;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            anim.SetTrigger("Bounce");
             collision.gameObject.GetComponent<MoveController>().Launch(force);
         }
     }
